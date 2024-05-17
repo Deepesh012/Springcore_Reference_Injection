4874048f03743717ff15a8eb06ffa4761233efeb
Deepesh Thakur <128272082+Deepesh012@users.noreply.github.com>
2024-05-17 17:04:18 +0530

    Add files via upload

---
+++ ref/B.java
@@ -0,0 +1,30 @@
+package com.springcore.ref;
+
+					//This is the class which is passed as an object to class A
+
+public class B {
+
+	private int y;
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public B() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "B [y=" + y + "]";
+	}
+
+	
+	
+	
+}
